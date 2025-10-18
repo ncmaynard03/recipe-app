dcf230ba3342d98056a610b3d459d6a93ea2f043
CalenJ <calenjones@rocketmail.com>
2025-10-18 16:42:52 -0500

    code successful authenticates google users

---
+++ src/components/auth/GoogleOAuth.tsx
@@ -0,0 +1,25 @@
+import { supabase } from "~/supabase";
+import "../../styling/google-btn.css"
+import GoogleBtn from "~/assets/googlebtn.png"
+
+export function GoogleOAuth(){
+    
+    const continueWithGoogle = async () => {
+        const { data } = await supabase.auth.signInWithOAuth({
+            provider: 'google',
+            options: {
+                redirectTo: "http://localhost:3000",
+                queryParams: {
+                access_type: 'offline',
+                prompt: 'consent',
+                },
+            },
+        });
+    };
+
+    return (
+        <img src={GoogleBtn} onClick={continueWithGoogle}/>
+    );
+}
+
+
