59192ed81ee1164fe1da0103a516eda4135d4470
CalenJ <calenjones@rocketmail.com>
2025-10-19 17:18:20 -0500

    finished tho untested function

--- src/utils/genRandomUN.ts
+++ src/utils/genRandomUN.ts
@@ -1,5 +1,24 @@
+import * as fs from 'fs';
 
 function generateRandomUsername(): string {
-    const cookingAdjectives = [];
-    const cookingNouns = [];
+
+    const adjFilePath: string = "../assets/names/cooking-adj.txt"
+    const nounFilePath: string = "../assets/names/cooking-noun.txt"
+
+    const adjFileContent: string = fs.readFileSync(adjFilePath, 'utf-8');
+    const nounFileContent: string = fs.readFileSync(nounFilePath, 'utf-8');
+
+    const cookingAdjs: string[] = adjFileContent.split('\n');
+    const cookingNouns: string[] = nounFileContent.split('\n');
+
+    const adj: string = cookingAdjs[Math.floor(Math.random() * cookingAdjs.length)];
+    const noun: string = cookingNouns[Math.floor(Math.random() * cookingNouns.length)];
+    const unNum: number = Math.floor(Math.random() * 1000);
+
+    const username = `${adj}${noun}${unNum}`;
+
+    return username;
 }
+
+let username = generateRandomUsername();
+console.log(username);
