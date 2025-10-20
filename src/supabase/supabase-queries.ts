d99277d1b5f48c59ac88864ad4b74c3e3b9c468d
CalenJ <calenjones@rocketmail.com>
2025-10-20 10:48:24 -0500

    added shells of queries (userExists, getCurrUserID, getPubRec, getUserRec

---
+++ src/supabase/supabase-queries.ts
@@ -0,0 +1,19 @@
+import { supabase } from "./supabase-client";
+import { generateRandomUsername } from "~/utils/genRandomUN";
+
+//Checks if the user already exists in Supabase (new or returning user)
+export async function userExists(userID: string){
+
+}
+
+export async function getCurrentUserID(){
+
+}
+
+export async function getPublicRecipes(){
+
+}
+
+export async function getUserRecipes(){
+
+}
