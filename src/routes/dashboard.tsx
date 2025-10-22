b39ef964d71a5148dca93291955f19c876ae0060
CalenJ <calenjones@rocketmail.com>
2025-10-21 19:28:03 -0500

    merge

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,14 +1,10 @@
 import { Title } from "@solidjs/meta";
 
-export default function Dashboard() {
-    return (
-        <main>
-            <Title>
-                Dashboard
-            </Title>
-            <h1>
-                Dashboard
-            </h1>
-        </main>
-    );
+export default function About() {
+  return (
+    <main>
+      <Title>My Recipe Box</Title>
+      <h1>This is the dashboard, just need a placeholder for now to reroute for login, make any and all changes needed, formatting and styling wise</h1>
+    </main>
+  );
 }
