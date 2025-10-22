1ec4e35fba051cf6d5d7aebe59c0cd86fcab7955
CalenJ <calenjones@rocketmail.com>
2025-10-21 20:14:48 -0500

    updated message

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -4,7 +4,8 @@ export default function About() {
   return (
     <main>
       <Title>My Recipe Box</Title>
-      <h1>This is the dashboard, just need a placeholder for now to reroute for login, make any and all changes needed, formatting and styling wise</h1>
+      <h4>Dashboard, style as needed, pushed for reroute</h4>
     </main>
+    
   );
 }
