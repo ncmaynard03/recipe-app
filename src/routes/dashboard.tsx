ac470842b7ca7ceaf8555e48cb6b82c76f5158bd
CalenJ <calenjones@rocketmail.com>
2025-10-23 15:50:12 -0500

    updated function name and added onMount to test random username and RLS, subject to change

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,10 +1,21 @@
 import { Title } from "@solidjs/meta";
+import { onMount, createSignal } from "solid-js";
+import { supabase } from "~/supabase/supabase-client";
+import * as supabaseFn from "~/supabase/supabase-queries";
 
-export default function About() {
+export default function Dashboard() {
+    const [username, setUsername] = createSignal("")
+  onMount(async () => {
+    const currUN = await supabaseFn.ensureUserExists();
+    setUsername(currUN);
+  });
+
+    
   return (
     <main>
       <Title>My Recipe Box</Title>
       <h4>Dashboard, style as needed, pushed for reroute</h4>
+      <h5>Testing: Hello {username()}</h5>
     </main>
     
   );
