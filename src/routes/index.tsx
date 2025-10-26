58ab67ecc60eb2e17ce5ac42b6b7bd5ae9169d54
CalenJ <calenjones@rocketmail.com>
2025-10-26 12:41:49 -0500

    updated image path

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,21 @@
 import { Title } from "@solidjs/meta";
 import SignInComp from "~/components/auth/SignInComp";
-import MainImg from "~/assets/main-img.jpg"
+import MainImg from "~/assets/cropped-main.jpg"
+import "../styling/index.css"
 
 
 export default function MainPage() {
   return ( 
     <main class='main-page'>
       <Title>Capstone Cooking</Title>
-      <div class='first-section'>
+      <div class='first-section-cont'>
         <div class='main-page-img'>
           <img src={MainImg}/>
         </div>
         <div class='page-name-section'>
           <p id='page-name'>capstone cooking</p>
+          <SignInComp />
         </div>
-        <SignInComp />
       </div>
     </main>
   );
