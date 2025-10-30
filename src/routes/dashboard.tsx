a392364c9e907e6cbe7e47d1c725b12c80c03d46
CalenJ <calenjones@rocketmail.com>
2025-10-30 14:46:13 -0500

    updated paths for imports

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -2,12 +2,12 @@ import { Title } from "@solidjs/meta";
 import { onMount, createSignal } from "solid-js";
 import { supabase } from "~/supabase/supabase-client";
 import * as supabaseFn from "~/supabase/supabase-queries";
-import "../styling/login-page/index.css";
+import "../styling/dashboard/dashboard.css";
 
-import TaskBar from "~/components/dashboard-taskbar";
-import RecipeViewer from "~/components/dashboard-recipeviewer";
-import RecipeSearchbar from "~/components/dashboard-searchbar";
-import RecipeBrowser from "~/components/dashboard-recipebrowser";
+import TaskBar from "~/components/dashboard/dashboard-taskbar";
+import RecipeViewer from "~/components/dashboard/dashboard-recipeviewer";
+import RecipeSearchbar from "~/components/dashboard/dashboard-searchbar";
+import RecipeBrowser from "~/components/dashboard/dashboard-recipebrowser";
 
 export default function Dashboard() {
   const [username, setUsername] = createSignal("")
