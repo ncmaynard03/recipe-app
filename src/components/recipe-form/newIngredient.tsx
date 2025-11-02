36ef5e17eb41498a110d461961df51037433f733
CalenJ <calenjones@rocketmail.com>
2025-11-02 08:53:51 -0600

    completed new ingredient row logic, mod to come

---
+++ src/components/recipe-form/newIngredient.tsx
@@ -0,0 +1,45 @@
+export function NewIngredient( props: {
+    ingredient: { quantity: string, units: string; ingredientName: string};
+    index: number;
+    onChange: (index: number, field: "quantity" | "units" | "ingredientName",
+               value: string) => void;
+
+}) {
+    const { ingredient, index, onChange } = props;
+
+    const usUnitsList = [
+                        { value: "pounds", label: "lb(s)" },
+                        { value: "ounces", label: "oz" },
+                        { value: "gallons", label: "gal(s)" },
+                        { value: "quarts", label: "qt(s)" },
+                        { value: "pints", label: "pt(s)" },
+                        { value: "cups", label: "cup(s)" },
+                        { value: "fluid-ounces", label: "fl. oz" },
+                        { value: "tablespoons", label: "tbsp(s)" },
+                        { value: "teaspoons", label: "tsp(s)" },
+                        { value: "pinch", label: "pinch" },
+                        { value: "box", label: "box" },
+                        { value: "can", label: "can" },
+                        { value: "packet", label: "packet" },
+    ];
+
+    return (
+        <div class="new-ing-row">
+            
+            {/*Quantity*/}
+            <input type="text" value={ingredient.quantity} placeholder="Qty" onInput={(event) => onChange(index, "quantity", event.currentTarget.value)}/>
+
+            {/* Units */}
+            <select value={ingredient.units} onInput={(event) => onChange(index, "units", event.currentTarget.value)}>
+                <option value="">Unit</option>
+                {usUnitsList.map((units) => (
+                    <option value={units.value}>{units.label}</option>
+                ))}
+            </select>
+
+            {/* Ingredient Name */}
+            <input type="text" value={ingredient.ingredientName} placeholder="Ingredient Name" onInput={(event) => onChange(index, "ingredientName", event.currentTarget.value)}/>
+        </div>
+    );
+    
+}
