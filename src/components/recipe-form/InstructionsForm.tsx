8de15e69f21da48cb5a82fc72d54b913ba0f269a
CalenJ <calenjones@rocketmail.com>
2025-11-02 14:20:29 -0600

    added delete button functionality to instructions

--- src/components/recipe-form/InstructionsForm.tsx
+++ src/components/recipe-form/InstructionsForm.tsx
@@ -15,6 +15,15 @@ export function InstructionsForm(){
         setInstructions(index, value);
     }
 
+    function deleteInstruction(index: number){
+        if (instructions.length > 1){
+            setInstructions(instructions.filter((_, insIndex) => insIndex !== index));
+        }
+        else {
+            return;
+        }
+    }
+
     return(
         <div class="instructions-form">
             <div class="instructions-lbl-cont">
@@ -23,12 +32,18 @@ export function InstructionsForm(){
 
             {/* Creates inital and addtional recipe instructions/steps */}
             <div class="def_ins">
-                <Index each={instructions}>
-                    {(step, index) => (
-                        <textarea placeholder={`Step ${index + 1}`} value={step()} onInput={(event) => updateInstruction(index, event.currentTarget.value)}/>
-                    )}
-                </Index>
-
+                <div class="ins-block">
+                    <Index each={instructions}>
+                        {(step, index) => (
+                            <div class="new-ins">
+                                <textarea placeholder={`Step ${index + 1}`} value={step()} onInput={(event) => updateInstruction(index, event.currentTarget.value)}/>
+                                <div class="delete-btn">
+                                    <button type="button" onClick={() => deleteInstruction(index)}>-</button>
+                                </div>
+                            </div>
+                        )}
+                    </Index>
+                </div>
             </div>
             <button id="add-ins-btn" type="button" onClick={addNewInstruction}>+</button>
         </div>
