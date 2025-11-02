98e7003f42ba9a19ea8d1fecfd83886d3b5bb018
CalenJ <calenjones@rocketmail.com>
2025-11-02 09:10:41 -0600

    added code to create new rows in instructions

---
+++ src/components/recipe-form/InstructionsForm.tsx
@@ -0,0 +1,34 @@
+import { createSignal, For } from "solid-js";
+
+export function InstructionsForm(){
+
+    const [instructions, setInstructions] = createSignal([""]);
+    
+    function addNewInstruction(){
+        setInstructions([...instructions(), ""]);
+    }
+
+    function updateInstruction( index: number, value: string){
+        const updatedIns = [...instructions()];
+        updatedIns[index] = value;
+        setInstructions(updatedIns);
+    }
+
+    return(
+        <div class="instructions-form">
+            <div class="instructions-lbl-cont">
+                <label>Recipe Instructions</label>
+            </div>
+            <div class="def_ins">
+                <For each={instructions()}>
+                    {(step, index) => (
+                        <textarea placeholder={`Step ${index() + 1}`} value={step} onInput={(event) => updateInstruction(index(), event.currentTarget.value)}/>
+                    )}
+                </For>
+
+            </div>
+            <button id="add-ins-btn" type="button" onClick={addNewInstruction}>+</button>
+        </div>
+    );
+
+}
