d4e288ae6d97e2dc1a3f6d93b74e8d6b5a8eaa48
sushimus <59678594+Sushimus@users.noreply.github.com>
2025-11-03 11:05:22 -0600

    Revert "pretty please"
    This reverts commit e8c6fee06a9c83c65fc893b068abcbd00863b4c2.

--- src/components/dashboard/dashboard-recipebrowser.tsx
+++ src/components/dashboard/dashboard-recipebrowser.tsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { createEffect, createSignal, For } from "solid-js";
 import "../../styling/dashboard/dashboard-recipebrowser.css";
 import { loadAllRecipes, Recipe, recipes, setRecipes } from "~/stores/recipes";
@@ -15,13 +14,8 @@ export default function RecipeBrowser() {
             await loadAllRecipes();
         }
     })
-=======
-import "../../styling/dashboard/dashboard-recipebrowser.css";
-import RecipeBrowsingItem from "./dashboard-recipebrowsingitem";
->>>>>>> Stashed changes
 
     return (
-<<<<<<< Updated upstream
         <div class="recipe-browser">
             <h2>Recipe Browser</h2>
             <div class="recipe-list">
@@ -41,25 +35,6 @@ import RecipeBrowsingItem from "./dashboard-recipebrowsingitem";
                 </For>
             </div>
 
-=======
-        <div class="browsing-container">
-            <div class="browsing-container-content">
-                <RecipeBrowsingItem />
-                <RecipeBrowsingItem />
-                <RecipeBrowsingItem />
-                <RecipeBrowsingItem />
-                <RecipeBrowsingItem />
-                <RecipeBrowsingItem />
-                <RecipeBrowsingItem />
-                <RecipeBrowsingItem />
-                <RecipeBrowsingItem />
-                <RecipeBrowsingItem />
-                <RecipeBrowsingItem />
-                <RecipeBrowsingItem />
-                <RecipeBrowsingItem />
-                <RecipeBrowsingItem />
-            </div>
->>>>>>> Stashed changes
         </div>
     );
 }
--- src/components/dashboard/dashboard-recipecard.tsx
+++ src/components/dashboard/dashboard-recipecard.tsx
@@ -1,29 +1,11 @@
+import { createSignal } from "solid-js";
 import "../../styling/dashboard/dashboard-recipeviewer.css";
-import { IngredientsForm } from "../recipe-form/IngredientsForm";
 
 export default function RecipeCard() {
     return (
         <div class="recipe-card">
-<<<<<<< Updated upstream
             {/* <button>spice level</button> */}
             {/* <button>why would you need anything but spice level</button> */}
-=======
-            <div class="recipe-card-contents">
-                <div class="time-fields">
-                    <div class="prep-time">
-                        <label>Prep Time:</label>
-                        <input type="text" id="prep-time" name="prep-time" />
-                        <label>mins</label>
-                    </div>
-                    <div class="cook-time">
-                        <label>Cook Time:</label>
-                        <input type="text" id="cook-time" name="cook-time" />
-                        <label>mins</label>
-                    </div>
-                </div>
-                <IngredientsForm />
-            </div>
->>>>>>> Stashed changes
         </div>
     );
 }
