e8c6fee06a9c83c65fc893b068abcbd00863b4c2
sushimus <59678594+Sushimus@users.noreply.github.com>
2025-11-03 10:59:52 -0600

    pretty please

--- src/components/dashboard/dashboard-taskbar.tsx
+++ src/components/dashboard/dashboard-taskbar.tsx
@@ -1,11 +1,21 @@
 import { createSignal } from "solid-js";
 import "../../styling/dashboard/dashboard-taskbar.css";
+<<<<<<< Updated upstream
 import { redirect, useNavigate } from "@solidjs/router";
+=======
+import pencil from "~/assets/dashboard/pencil.png";
+import globe from "~/assets/dashboard/globe.png";
+import tag from "~/assets/dashboard/tag.png";
+import markdown from "~/assets/dashboard/markdown.png";
+import output from "~/assets/dashboard/output.png";/*export is a reserved word*/
+import bin from "~/assets/dashboard/trash-can.png";
+>>>>>>> Stashed changes
 
 export default function TaskBar() {
     const navigate = useNavigate();
     return (
         <div class="task-bar">
+<<<<<<< Updated upstream
             <div class="task-bar-buttons" >
                 <button onClick={() => navigate("/add-recipe")}>new recipe</button>
                 <button>toggle edit</button>
@@ -14,6 +24,18 @@ export default function TaskBar() {
                 <button>toggle public</button>
                 <button>export</button>
                 <button>delete</button>
+=======
+            <div class="task-bar-buttons">
+                <button><img src={pencil}/></button>
+                <button><img src={tag}/></button>
+                <button><img src={markdown}/></button>
+                <button><img src={globe}/></button>
+            </div>
+
+            <div class="task-bar-buttons">
+                <button><img src={output}/></button>
+                <button><img src={bin}/></button>
+>>>>>>> Stashed changes
             </div>
         </div>
     );
