ce61cdc94b3b9d3f725188a1aa4efe9acb6d39d7
sushimus <59678594+Sushimus@users.noreply.github.com>
2025-11-03 10:58:36 -0600

    please, i will cry

--- src/routes/accountdelete.tsx
+++ src/routes/accountdelete.tsx
@@ -1,14 +1,21 @@
-import { Title } from "@solidjs/meta";
+import "~/styling/account-delete/account-delete.css";
 
 export default function AccountDelete() {
     return (
         <main>
-            <Title>
-                Delete Account
-            </Title>
-            <h1>
-                No dont leave youre so sexy
-            </h1>
+            <div class="accountdelete-container">
+                <div class="accountdelete-content">
+                    <h1>CONFIRM</h1>
+                    <h3>
+                        Are you sure you want to delete your account?<br />
+                        (This makes our interns very sad)<br />
+                    </h3>
+                    <div class="delete-buttons">
+                        <button>Delete Account</button>
+                        <button>Cancel</button>
+                    </div>
+                </div>
+            </div>
         </main>
     );
 }
