63c0741d41de50d3ad40a6678356718912c0d64d
CalenJ <calenjones@rocketmail.com>
2025-11-14 11:13:45 -0600

    added content to file

---
+++ src/components/screens/CreateEditScreen.tsx
@@ -0,0 +1,17 @@
+export function CreateEditScreen(){
+
+
+    return(
+        <div class="ce-screen">
+            <p>Choose A Recipe Below to Edit</p>
+            <div class="existing-recipe-tiles"></div>
+            <p>or</p>
+            <div class="new-recipe-tile">
+                <button>+</button>
+                <p>New Recipe</p>
+            </div>
+            <p>Click The Box Above to Create a New Recipe</p>
+        </div>
+    );
+
+}
