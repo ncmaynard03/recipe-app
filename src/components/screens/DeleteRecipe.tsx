04c4b93dffaa861834abba3699599f84cdd2f120
CalenJ <calenjones@rocketmail.com>
2025-11-20 19:53:51 -0600

    imported css and updated label

--- src/components/screens/DeleteRecipe.tsx
+++ src/components/screens/DeleteRecipe.tsx
@@ -1,8 +1,12 @@
+import "~/styling/screens/delete-screen.css"
+
 export default function DeleteRecipe() {
 
     return(
         <div class="delete-screen-cont">
-            <p>Choose the Recipes You Wish To Delete</p>
+            <div class="delete-lbl">
+                <p>Choose the Recipe(s) You Wish To Delete</p>
+            </div>
         </div>
     );
 }
