3a9714d80ca29720e4f077b260625f7bcec9b544
CalenJ <calenjones@rocketmail.com>
2025-11-20 19:44:36 -0600

    init commit and added lbl

---
+++ src/components/screens/DeleteRecipe.tsx
@@ -0,0 +1,8 @@
+export default function DeleteRecipe() {
+
+    return(
+        <div class="delete-screen-cont">
+            <p>Choose the Recipes You Wish To Delete</p>
+        </div>
+    );
+}
