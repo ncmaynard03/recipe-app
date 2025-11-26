d0ebac1be3be4ad504f23d9a942b76f20a686008
CalenJ <calenjones@rocketmail.com>
2025-11-26 14:19:08 -0600

    created panes for result and search options and search bar

--- src/components/screens/SearchRecipe.tsx
+++ src/components/screens/SearchRecipe.tsx
@@ -1,17 +1,24 @@
 import Arrow from "~/assets/arrow.png";
+import Hat from "~/assets/chef_hat.png"
+import MGlass from "~/assets/dashboard/magnifying-glass.png"
 import "~/styling/screens/search-recipe.css"
 
 export default function SearchRecipe(){
     return(
         <div class="search-page-cont">
-            <div class="search-bar-row">
-                <input type="text" placeholder="Search All Recipes..."/>
-                <div class="submit">
-                    <button><img src={Arrow}/></button>
+            <div class="search-results-section">
+                <div class="default-search-view">
+                    <img src={Hat}/>
+                    <p>Results Will Display Here</p>
                 </div>
             </div>
-            <div class="adv-btn">
-                <button>Advanced Search</button>
+            <div class="search-options-pane">
+                <div class="search-bar-row">
+                    <div class="search-field">
+                        <img src={MGlass}/>
+                        <input type="text" placeholder="Search All Recipes..."/>
+                    </div>
+                </div>
             </div>
         </div>
     );
