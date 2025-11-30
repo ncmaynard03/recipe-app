071aea0528b53acc78e65dd6d4e2987d2db1e6b5
CalenJ <calenjones@rocketmail.com>
2025-11-30 14:48:15 -0600

    added clear button to search box and search results screen when typing in text field

--- src/components/screens/SearchRecipe.tsx
+++ src/components/screens/SearchRecipe.tsx
@@ -1,22 +1,35 @@
+import { createSignal, Show } from "solid-js";
 import Arrow from "~/assets/arrow.png";
 import Hat from "~/assets/chef_hat.png"
 import MGlass from "~/assets/dashboard/magnifying-glass.png"
 import "~/styling/screens/search-recipe.css"
 
 export default function SearchRecipe(){
+    const [searchText, setSearchText] = createSignal("");
+    const clearSearch = () => setSearchText("");
+
     return(
         <div class="search-page-cont">
             <div class="search-results-section">
-                <div class="default-search-view">
-                    <img src={Hat}/>
-                    <p>Results Will Display Here</p>
-                </div>
+                <Show when={searchText() !== ""} fallback = {
+                    <div class="default-search-view">
+                        <img src={Hat}/>
+                        <p>Results Will Display Here</p>
+                    </div>}
+                >
+                    <div class="search-results">
+                        <p>Found recipes</p>
+                    </div>
+                </Show>
             </div>
             <div class="search-options-pane">
                 <div class="search-bar-row">
                     <div class="search-field">
                         <img src={MGlass}/>
-                        <input type="text" placeholder="Search All Recipes..."/>
+                        <input type="text" placeholder="Search All Recipes..." value={searchText()} onInput={(query) => setSearchText(query.currentTarget.value)}/>
+                        <Show when={searchText().trim() !== ""}>
+                            <button class="clear-btn" onClick={clearSearch}>clear</button>
+                        </Show>
                     </div>
                 </div>
                 
