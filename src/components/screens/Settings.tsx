ebdc4f617d531381e594527e96c1bba528d8740a
CalenJ <calenjones@rocketmail.com>
2025-12-08 10:49:51 -0600

    settings page initial content

---
+++ src/components/screens/Settings.tsx
@@ -0,0 +1,35 @@
+import "~/styling/screens/settings-screen.css"
+import { onMount, createSignal } from "solid-js";
+import * as supabaseFn from "~/supabase/supabase-queries";
+
+export default function Settings() {
+    const [username, setUsername] = createSignal("");
+
+    onMount(async () => {
+        const currUN = await supabaseFn.ensureUserExists();
+        setUsername(currUN || "");
+    });
+
+    return (
+        <div class="settings-page-cont">
+            <div class="settings-pane">
+                <div class="pane-title-cont">
+                    <h2>Account Settings</h2>
+                </div>
+                <div class="pane-content-cont">
+                    <p>Username</p>
+
+                    <input type="text" value={username()} 
+                        onInput={(e) => setUsername(e.currentTarget.value)} 
+                    />
+                    <div class="save-btn">
+                        <button>Save Username</button>
+                    </div>
+                    <div class="delete-acct-btn">
+                        <button>Delete Account</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
