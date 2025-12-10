852c8b0e464536407246bb12bc3c9f6109b7068b
Nathan Maynard <ncmaynard03@gmail.com>
2025-12-10 09:44:41 -0600

    host fix

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,4 @@
 import { defineConfig } from "@solidjs/start/config";
-import netlify from "@solidjs/start/netlify";
 
 export default defineConfig({
-    server: {
-        adapter: netlify()
-    }
 });
