026204251a084f937a86287885dcaf9608d4d549
Nathan Maynard <ncmaynard03@gmail.com>
2025-12-10 07:58:10 -0600

    host fix

--- src/components/auth/GoogleOAuth.tsx
+++ src/components/auth/GoogleOAuth.tsx
@@ -2,23 +2,14 @@ import { supabase } from "~/supabase/supabase-client";
 import GoogleBtn from "~/assets/web_dark_sq_ctn@2x.png"
 
 
-export function GoogleOAuth(){
-    
+export function GoogleOAuth() {
+
     const continueWithGoogle = async () => {
-        const { data, error } = await supabase.auth.signInWithOAuth({
-            provider: 'google',
-            options: {
-                redirectTo: "http://localhost:3000/dashboard",
-                queryParams: {
-                access_type: 'offline',
-                prompt: 'consent',
-                },
-            },
-        });
+        const { data, error } = await supabase.auth.signInWithOAuth({ provider: 'google' });
     };
 
     return (
-        <img src={GoogleBtn} onClick={continueWithGoogle}/>
+        <img src={GoogleBtn} onClick={continueWithGoogle} />
     );
 }
 
